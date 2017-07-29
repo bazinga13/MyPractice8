11774851c27460458dabad99e49a438ca29245d5
ianni <iannistsoumas@gmail.com>
2017-07-29 03:52:06 +0300

    korelko practice 8

--- app/src/main/java/korelkoproject/ianni/mypractice8/MainActivity.java
+++ app/src/main/java/korelkoproject/ianni/mypractice8/MainActivity.java
@@ -1,13 +1,30 @@
 package korelkoproject.ianni.mypractice8;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+     int display_mode = getResources().getConfiguration().orientation;
+
+        if (display_mode == Configuration.ORIENTATION_LANDSCAPE) {
+
+            this.getSupportActionBar().hide();
+
+        }
+        else {
+
+            this.getSupportActionBar().show();
+
+        }
+
         setContentView(R.layout.activity_main);
     }
+
+
 }
